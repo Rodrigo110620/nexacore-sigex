6dc7968a40408092f9af3c6297e89954a9311fe6
Lia Cardenas <liacardenas@gmail.com>
2026-09-15 19:17:57 -0400

    feat(hu2-frontend):pantalla de registro de usuarios

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -3,6 +3,7 @@ import { useAuth } from '../context/AuthContext'
 import LoginPage from '../pages/Login/LoginPage'
 import DashboardPage from '../pages/Dashboard/DashboardPage'
 import ProtectedRoute from './ProtectedRoute'
+import UsuariosPage from '../pages/panel_admin/UsuariosPage'   // 👈 AGREGAR
 
 function LoginRoute() {
   const { isAuthenticated } = useAuth()
@@ -23,6 +24,9 @@ export default function AppRouter() {
             </ProtectedRoute>
           }
         />
+
+        {/* Ruta temporal para probar registro*/}
+        <Route path="/test-modal" element={<UsuariosPage />} />
       </Routes>
     </BrowserRouter>
   )
